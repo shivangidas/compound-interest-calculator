dae179738dfd94977c05486f0f876941862c7d1f
Shivangi <shivangi.das7@gmail.com>
2024-12-06 12:08:20 +0000

    calculate from simple interest

--- app/calculator.ts
+++ app/calculator.ts
@@ -5,12 +5,24 @@
 * n: number of times per year interest compounds (1-yearly, 12-monthly)
 * periodic_principal: regular amount deposited after the initial principal
 */
-
+export function simpleInterestAmount(principal: number, rate: number, time: number) {
+    return (principal * (1 + rate * time)).toFixed(2);
+}
 export function amountWithInitialDeposit(principal: number, rate: number, time: number, n: number) {
-    return principal * ((1 + rate / n) ** (n * time));
+    return (principal * ((1 + rate / n) ** (n * time))).toFixed(2);
 }
 
 export function amountWithContributions(principal: number, rate: number, time: number, n: number, periodic_principal: number) {
-    const amount = amountWithInitialDeposit(principal, rate, time, n)
-    return amount + ((periodic_principal * (((1 + (rate / n)) ** (time * n)) - 1)) / (rate / n))
+    const amount = Number(amountWithInitialDeposit(principal, rate, time, n))
+    return (amount + ((periodic_principal * (((1 + (rate / n)) ** (time * n)) - 1)) / (rate / n))).toFixed(2);
+}
+
+export function calculateWithoutFormula(principal: number, rate: number, time: number, n: number, periodic_principal: number) {
+
+    let amount = Number(simpleInterestAmount(principal, rate, 1 / n));
+    for (let i = 2; i <= (time * n); i++) {
+        let newPrincipal = amount + periodic_principal;
+        amount = Number(simpleInterestAmount(newPrincipal, rate, 1 / n));
+    }
+    return (amount + periodic_principal).toFixed(2);
 }
--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -1,15 +1,23 @@
-import { amountWithInitialDeposit, amountWithContributions } from "../app/calculator.js";
+import { amountWithInitialDeposit, amountWithContributions, calculateWithoutFormula } from "../app/calculator.js";
 
 describe('compound interest', () => {
     test('amountWithInitialDeposit', () => {
-        expect(amountWithInitialDeposit(1000, 0.05, 2, 1)).toBe(1102.5);
-        expect(amountWithInitialDeposit(1000, 0.05, 20, 1)).toBe(2653.2977051444223);
-        expect(amountWithInitialDeposit(1000, 0.05, 20, 12)).toBe(2712.6402854819885);
+        expect(amountWithInitialDeposit(1000, 0.05, 2, 1)).toBe("1102.50");
+        expect(amountWithInitialDeposit(1000, 0.05, 20, 1)).toBe("2653.30");
+        expect(amountWithInitialDeposit(1000, 0.05, 20, 12)).toBe("2712.64");
     });
 
     test('amountWithContributions', () => {
-        expect(amountWithContributions(1000, 0.05, 2, 1, 500)).toBe(2127.5);
-        expect(amountWithContributions(1000, 0.05, 2, 12, 500)).toBe(13697.901602557551);
-        expect(amountWithContributions(1000, 0.05, 20, 12, 500)).toBe(208229.47454332063);
+        expect(amountWithContributions(1000, 0.05, 2, 1, 500)).toBe("2127.50");
+        expect(amountWithContributions(1000, 0.05, 2, 12, 500)).toBe("13697.90");
+        expect(amountWithContributions(1000, 0.05, 20, 12, 500)).toBe("208229.47");
     });
-})
+});
+describe('compound interest without formula', () => {
+
+    test('calculateWithoutFormula', () => {
+        expect(calculateWithoutFormula(1000, 0.05, 2, 1, 500)).toBe("2127.50");
+        expect(calculateWithoutFormula(1000, 0.05, 2, 12, 500)).toBe("13697.91");
+        expect(calculateWithoutFormula(1000, 0.05, 20, 12, 500)).toBe("208229.62");
+    });
+});
